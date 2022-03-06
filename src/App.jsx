c1fc8a7da13b71aa0c7a0458f0260f8e1b6abda7
bashar125824 <bashar.alrefae.7@gmail.com>
2022-03-06 18:06:56 +0300

    class-19 Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home.jsx";
-//import Favlist from "./components/favlist/Favlist";
+import Favlist from "./components/favlist/Favlist";
 //import Modalmovie from "./components/modalmovie/Modalmovie";
 //import Movie from "./components/movie/Movie";
 //import Movielist from "./components/movielist/Movielist";
-//import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 
 export default function App() {
     console.log("ERROR");
     return (
+
         <Routes>
-            <Route path="/" element={<Home />} />
             
+            <Route path="/" element={<Home />} />
+            <Route path="/fav" element={<Favlist />} />
+
 
         </Routes>
     );
